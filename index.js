ea06a2d5790f15c0143204982b50fc6669d99486
LindsayMuji <lmelissa.mp@gmail.com>
2022-01-10 18:37:39 -0500

    Verificacion de ruta

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
-module.exports = () => {
+/*module.exports = () => {
   // ...
-};
+};*/
+import * as fs from 'fs';
+import * as path from 'path';
+
+let fileRoute='C://Users//melis//OneDrive//EScritorio//MDLinks//LIM016-md-links//Carpeta Prueba';
+
+// check if the route exist?
+let fileExist = fs.existsSync(fileRoute)? console.log("El archivo EXISTE!"):console.log("El archivo NO EXISTE!");
+// convert route in absolute
+let absoluteRoute = path.isAbsolute(fileRoute) ? path.resolve(fileRoute) : fileRoute
+console.log(absoluteRoute);
+//check if it is a Directory
+let isDirectory = fs.lstatSync(fileRoute).isDirectory();
+  console.log(isDirectory);
+//Read Directory
+let readDirectory = fs.readFile(fileRoute,{encoding:"utf-8"})? console.log(readDirectory):console.log(error.message);
+//check if it is a File
+let file = fs.lstatSync(fileRoute).isFile();
+  console.log(file);
